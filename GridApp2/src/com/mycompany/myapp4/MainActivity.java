6c7e0aafd453dac0eb4603b8a69116430967d5da
Vipul Kohli <vkohli678@yahoo.com>
2013-12-10 15:44:01 -0600

    Fixed broken grid activity

--- GridApp2/src/com/mycompany/myapp4/MainActivity.java
+++ GridApp2/src/com/mycompany/myapp4/MainActivity.java
@@ -21,4 +21,53 @@ public class MainActivity extends Activity
         setContentView(R.layout.main);
 		mSpin = (Spinner) findViewById(R.id.spin);
 		final String [] spinOps = {"Find by Category", "Find by Building"};
-		Stri
+		String [] saleCats = {"Apps", "Books", "Calculators", "Laptops", "Tablets", "Furniture", "Phones", "Roommates"};
+		String [] buildNames = {"All SMU", "All ResHalls", "Perkins", "Smith", "McElvaney", "Cockrell-McIntoch", "Greek", "Morrison-McGinnis","HTSC"};
+		mGrid= (GridView) findViewById(R.id.grid);
+		ArrayAdapter< String> ad = new ArrayAdapter < String > (this, android.R.layout.simple_spinner_item, spinOps);
+		mCatsAdapter = new GridAdapter(saleCats);
+		mBuildsAdapter = new GridAdapter(buildNames);
+		mGrid.setAdapter(mCatsAdapter);
+		mSpin.setAdapter(ad);
+		mSpin.setOnItemSelectedListener(spinListener);
+    }
+	private OnItemSelectedListener spinListener = new OnItemSelectedListener(){
+		public void onItemSelected(AdapterView<?>parent, View v, int position, long id)
+		{
+			//Toast.makeText(getApplicationContext(), "hi", Toast.LENGTH_LONG);
+			if (position > 0)
+				mGrid.setAdapter(mBuildsAdapter);
+			else
+			    mGrid.setAdapter(mCatsAdapter);
+		}
+		public void onNothingSelected(AdapterView<?>parent)
+		{
+
+		}
+	};
+	public class GridAdapter extends BaseAdapter{
+		private String[]gridWords;
+		public GridAdapter(String[]inWords)
+		{
+			gridWords = inWords;
+		}
+		public int getCount(){
+			return gridWords.length;
+		}
+		public Object getItem(int pos){
+			return gridWords[pos];
+		}
+		public long getItemId(int position){
+			return position;
+		}
+		public View getView(int position, View convertView, ViewGroup parent){
+			LayoutInflater in = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+			View view = in.inflate(R.layout.grid_item, null);
+			TextView tv = (TextView) view.findViewById(R.id.grid_item);
+			ImageView iv = (ImageView) view.findViewById(R.id.iconLink);
+			tv.setText(gridWords[position]);
+			iv.setImageResource(R.drawable.ic_launcher);
+			return view;
+		}
+	}
+}
