32b3fdad1231dcfea6537c615d82a23fd6280ee9
Vipul Kohli <vkohli@smu.edu>
2013-12-12 01:28:46 -0600

    Attempt to integrate tabs, ListView, and Camera

---
+++ QuickSnap-master/Camera/src/com/allthelucky/common/view/sample/test.java
@@ -0,0 +1,37 @@
+package com.allthelucky.common.view.sample;
+
+
+import com.allthelucky.common.view.*;
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import java.lang.*;
+import android.view.*;       
+import android.app.*;       
+import android.os.*;
+import android.content.*; 
+import android.widget.*;
+import android.widget.AdapterView.*;
+import android.view.View.*;
+import com.allthelucky.common.view.R;
+import com.allthelucky.common.view.ViewPagerIndicatorView;
+
+/**
+ * @author Vipul Kohli
+ * Click listeners, ListView
+ *
+ * @author savant-pan (viewPagerIndicatorView)
+ * 
+ */
+public class test extends Activity{
+	private ViewPagerIndicatorView viewPagerIndicatorView, mTabs;
+	private ListView mList;
+	private ArrayAdapter mOps1, mOps2, mOps3, mOps4, mOps5;
+	private String[] mCampus, mNil, mMainSale, mMainPlaces;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sample);
+		
+}}
+
